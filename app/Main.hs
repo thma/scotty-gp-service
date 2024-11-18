e1dfa0076f3a5c0828d4d740da7b597c6b18a029
Thomas Mahler <thomas.mahler@ista.com>
2024-11-18 10:14:06 +0100

    using a connection pool works nicely

--- app/Main.hs
+++ app/Main.hs
@@ -4,17 +4,14 @@
 module Main (main) where
 
 import           Control.Monad.IO.Class (liftIO)
-import           Database.GP            (TxHandling (..), allEntries, connect,
-                                         defaultSqliteMapping, deleteById,
-                                         insert, insertMany, select, selectById,
-                                         setupTable, update)
+import           Database.GP            hiding (delete)
 import           Database.HDBC.Sqlite3  (connectSqlite3)
 import           Models
 import           Network.HTTP.Types     (status404)
 import           Web.Scotty             (ActionM, captureParam, delete, get,
                                          json, jsonData, post, put, raiseStatus,
                                          scotty)
-
+                                                  
 -- Sample product list
 initialProducts :: [Product]
 initialProducts =
@@ -22,28 +19,37 @@ initialProducts =
     Product 2 "Product 2" "Description 2" 29.99
   ]
 
+-- Create a connection pool to the SQLite database
+sqlLitePool :: FilePath -> IO ConnectionPool
+sqlLitePool sqlLiteFile = createConnPool AutoCommit sqlLiteFile connectSqlite3 10 100  
+
+-- Helper function to run a database action with a connection from the pool
+withConnFrom :: ConnectionPool -> (Conn -> IO a) -> ActionM a
+withConnFrom pool gpAction = liftIO $ withResource pool gpAction
+
 main :: IO ()
 main = do
-  -- connect to a database in auto commit mode
-  conn <- connect AutoCommit <$> connectSqlite3 "sqlite.db"
+  -- create a connection pool to the SQLite database
+  pool <- sqlLitePool "sqlite.db"
+  let withPooledConn = withConnFrom pool
 
-  -- initialize Product table
-  _ <- setupTable @Product conn defaultSqliteMapping
-
-  -- insert initial products
-  _ <- insertMany conn initialProducts
+  withResource pool $ \conn -> do
+    -- initialize Product table
+    setupTable @Product conn defaultSqliteMapping
+    -- insert initial products
+    insertMany conn initialProducts
 
   -- Start the web server
   scotty 3000 $ do
     -- Define a route to list all products
     get "/products" $ do
-      products <- liftIO $ select @Product conn allEntries
+      products <- withPooledConn $ \conn -> select @Product conn allEntries
       json products
 
     -- Define a route to get a product by ID
     get "/products/:id" $ do
       productIdParam <- captureParam "id" :: ActionM Int
-      prod <- liftIO $ selectById @Product conn productIdParam
+      prod <- withPooledConn $ \conn -> selectById @Product conn productIdParam
       case prod of
         Just p  -> json p
         Nothing -> raiseStatus status404 "not found"
@@ -51,7 +57,7 @@ main = do
     -- Define a route to create a new product
     post "/products" $ do
       newProduct <- jsonData
-      insertedProduct <- liftIO $ insert @Product conn newProduct
+      insertedProduct <- withPooledConn $ \conn -> insert @Product conn newProduct
       json insertedProduct
 
     -- Define a route to update a product by ID
@@ -59,11 +65,12 @@ main = do
       productIdParam <- captureParam "id"
       updatedProduct <- jsonData
       let updatedProductWithId = updatedProduct {id = productIdParam}
-      updated <- liftIO $ update @Product conn updatedProductWithId
+      updated <- withPooledConn $ \conn -> upsert @Product conn updatedProductWithId
       json updated
 
     -- Define a route to delete a product by ID
     delete "/products/:id" $ do
       productIdParam <- captureParam "id" :: ActionM Int
-      deleted <- liftIO $ deleteById @Product conn productIdParam
+      deleted <- withPooledConn $ \conn -> deleteById @Product conn productIdParam
       json deleted
+
